45ced4bb398b0cacace76f68245fd3062a3c91d6
Trevor Porter <typorter@godaddy.com>
2016-08-01 09:54:01 -0700

    Initial Commit

---
+++ FabricTwitterKit/FabricTwitterKit.h
@@ -0,0 +1,13 @@
+//
+//  FabricTwitterKit.h
+//  FabricTwitterKit
+//
+//  Created by Trevor Porter on 8/1/16.
+//  Copyright © 2016 Trevor Porter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FabricTwitterKit : NSObject
+
+@end
---
+++ FabricTwitterKit/FabricTwitterKit.m
@@ -0,0 +1,13 @@
+//
+//  FabricTwitterKit.m
+//  FabricTwitterKit
+//
+//  Created by Trevor Porter on 8/1/16.
+//  Copyright © 2016 Trevor Porter. All rights reserved.
+//
+
+#import "FabricTwitterKit.h"
+
+@implementation FabricTwitterKit
+
+@end
