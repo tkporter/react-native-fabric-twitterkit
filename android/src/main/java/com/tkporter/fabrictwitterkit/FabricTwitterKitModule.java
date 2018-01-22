fd0834566af60564f80f0ed948fbde520f5e5db3
Ryan Leckey <leckey.ryan@gmail.com>
2018-01-22 14:07:56 -0800

    Upgrade TwitterKit for Android (#31)
    * [TwitterKit 3.x] logOut -> clearActiveSession
    
    * [TwitterKit 3.x] Upgrade dependencies in build.gradle

--- android/src/main/java/com/tkporter/fabrictwitterkit/FabricTwitterKitModule.java
+++ android/src/main/java/com/tkporter/fabrictwitterkit/FabricTwitterKitModule.java
@@ -192,7 +192,7 @@ public class FabricTwitterKitModule extends ReactContextBaseJavaModule implement
 
     @ReactMethod
     public void logOut() {
-        TwitterCore.getInstance().logOut();
+        TwitterCore.getInstance().getSessionManager().clearActiveSession();
     }
 
     private boolean hasValidKey(String key, ReadableMap options) {
